020a0c9e7d083c5ac886281fa8cc60075a6fd3e0
minate <32261757+Rajkalp@users.noreply.github.com>
2019-10-01 02:10:45 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 	google: {
-		clientID:'867330616214-p26h0gsarv543139glekb13ibcd5uoe8.apps.googleusercontent.com',
-		clientSecret: 'gUsjD7xXc_JOGdmywiL4cgkq',
+		clientID:'google client id',
+		clientSecret: 'google secret id',
 	},
 	mongodb:{
-		dbURI:'mongodb://localhost:27017/mydb'
+		dbURI:'mongodb url'
 	},
 	session:{
-		cookieKey:'abcdefhtjasbkd'
+		cookieKey:'any characters for cookie encryption'
 	}
 }
